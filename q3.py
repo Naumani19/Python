068692b29c79cfec65ae0473810c75ec1434f4fe
Sameer Naumani <snaumani97@gmail.com>
2019-02-18 20:33:44 -0500

    test

--- q3.py
+++ q3.py
@@ -1,23 +1,66 @@
-#q4
+def layerTopRight(matrix):
+  
+  # remove and store the first row from matrix
+  top = matrix.pop(0)
+  
+  # store the right column of the matrix
+  right = []
+  
+  # remove the last column from the matrix
+  for i in range(0, len(matrix)):
+    e = matrix[i].pop()
+    right.append(e)
+    
+  # return the top row and last column elements as a list
+  return top + right
 
+def layerBottomLeft(matrix):
+  
+  # remove and store the last row from matrix in reverse order
+  bottom = matrix.pop()[::-1]
+  
+  # store the left column of the matrix
+  left = []
+  
+  # remove the first column from the matrix
+  for i in range(0, len(matrix)):
+    e = matrix[i].pop(0)
+    left.append(e)
+    
+  # return the top row and last column elements as a list
+  return bottom + left[::-1]
 
-#q2
-#Write a function swap(s) that takes a string as an argument and swaps every-other character 
-#Example swap('hello world')
-#ehll oowlrd  
+# our main spiral function that will
+# return a final spiral ordered list
+def spiral(matrix):
+  
+  # where we store our final spiraled list
+  spir = []
+  
+  while len(matrix) > 0:
     
-def swap2(s):
-  odds = ""
-  evens = ""
-  c = list(s)
-  for index in c:
-    s[index], s[index+1] = s[index + 1], s[index] 
-    ''.join.(c)
+    # if only 1 more element left in matrix
+    if len(matrix) == 1:
+      spir += matrix[0]
+      break
+      
+    # return the spiraled list of the top row and
+    # right column for this matrix
+    tr = layerTopRight(matrix)
+    spir += tr
     
-    #  evens = evens + s[index]
-    #if not (index %2 == 0):
-     #  odds = odds + s[index]
-    #pile = evens + odds
-    #print pile  
-  #print pile
+    # return the spiraled list of the bottom row and
+    # left column for this matrix
+    bl = layerBottomLeft(matrix)
+    spir += bl
     
+  return spir
+  
+  
+# setup a matrix  
+M = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+# return it in spiral order
+print spiral(M)     
